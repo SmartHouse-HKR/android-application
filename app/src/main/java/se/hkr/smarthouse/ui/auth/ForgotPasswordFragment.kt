e0f374ea9dc8382ea0735fc68207defbec863c78
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2019-10-23 18:32:53 +0200

    Add auth fragments
    Add all fragments related to the auth part of the application. The main
    launcher fragment is the first thing the user interacts with. The other
    fragments are responsible for what the name implies.
    
    Add colours and some string resources to support all the new  fragments.
    
    Add the initial classes that match those fragment without any
    implementation yet.

---
+++ app/src/main/java/se/hkr/smarthouse/ui/auth/ForgotPasswordFragment.kt
@@ -0,0 +1,17 @@
+package se.hkr.smarthouse.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import se.hkr.smarthouse.R
+
+class ForgotPasswordFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    }
+}
