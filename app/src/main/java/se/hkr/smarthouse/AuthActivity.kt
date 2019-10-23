e81e86c057f4ffe89aec4f23b2825cd9a95dd2cb
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2019-10-23 13:51:29 +0200

    Add auth activity Class and layout

---
+++ app/src/main/java/se/hkr/smarthouse/AuthActivity.kt
@@ -0,0 +1,11 @@
+package se.hkr.smarthouse
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
