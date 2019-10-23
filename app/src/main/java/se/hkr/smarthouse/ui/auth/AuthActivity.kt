20bc538ad3d6ee030dbf57e8152fb07b5896861c
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2019-10-23 16:07:09 +0200

    Update package structure
    "ui" package is used to handle everything that has to do with the
    user interface. Inside it, there will be further division in more
    packages to bundle up the code in logical parts. Initially, the login
    part of the application will reside inside "auth" and later there will
    be new packages holding any other parts of the application.

--- app/src/main/java/se/hkr/smarthouse/ui/auth/AuthActivity.kt
+++ app/src/main/java/se/hkr/smarthouse/ui/auth/AuthActivity.kt
@@ -1,7 +1,8 @@
-package se.hkr.smarthouse
+package se.hkr.smarthouse.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import se.hkr.smarthouse.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
